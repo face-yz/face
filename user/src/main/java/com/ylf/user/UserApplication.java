7fff631e0e96d68b3912d6e82ffe9f07df12f085
ylf-github <18629450764@163.com>
2019-12-10 23:04:51 +0800

    arrange baiduAPI

---
+++ user/src/main/java/com/ylf/user/UserApplication.java
@@ -0,0 +1,19 @@
+package com.ylf.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan(
+        "com.ylf.user.daoAPI"
+        )
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
